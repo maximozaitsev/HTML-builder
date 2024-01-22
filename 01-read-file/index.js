f7d1e38617f2eb9e9a213a7ad808954790068acd
Maxim <104011221+maximozaitsev@users.noreply.github.com>
2024-01-22 10:39:09 +0300

    feat: resolve 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+readStream.on('error', (err) => {
+  console.error(`An error occurred: ${err.message}`);
+});
